eee63646cf5b8e0c2ff50cada205c599d3ba4108
Jop Peters <jop@mastercoding.nl>
2023-01-06 15:23:57 +0100

    Fix formatter

--- src/validator/ch/validator.ts
+++ src/validator/ch/validator.ts
@@ -4,9 +4,15 @@ import { Pattern } from '../pattern';
 
 const patterns = [
   {
-    regexp: /^([A-Z]{1,2})([0-9]{1,6})[ZU]?$/,
+    regexp: /^([A-Z]{1,2})([0-9]{1,6})$/,
     formatter(matches): string {
       return matches[1] + ' ' + matches[2];
+    },
+  },
+  {
+    regexp: /^([A-Z]{1,2})([0-9]{1,6})([ZU])$/,
+    formatter(matches): string {
+      return matches[1] + ' ' + matches[2] + ' ' + matches[3];
     },
   },
 ];
